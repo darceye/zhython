ea32f2a1ede9958e8e3f012eea97ba92c508ba80
littleprog <littleprog@foxmail.com>
2020-03-28 07:33:56 +0800

    增加string检测函数

--- test/test_trans.py
+++ test/test_trans.py
@@ -18,22 +18,63 @@ pass
     src_array = trans.splitComment(src)
     assert src_array == ['\n', '# comment', '\npass\n   ', '# comment', '\npass ', '# comment', '\npass', '#comment', '\n\'\'\'\ncomment\n\'\'\'\npass\n"""\ncomment\n"""\npass\n    ']
 
-def testSplitMultilineString():
-    src = '''
-# comment
-pass
-   # comment
-pass # comment
-pass#comment
+def testSplitMultilineString1():
+    src = '''qwer
 \'\'\'
 comment
 \'\'\'
-pass
+asdf'''
+    assert trans.splitMultilineString(src) == ['qwer\n',"'''\ncomment\n'''", '\nasdf']
+
+def testSplitMultilineString2():
+    src = '''qwer
+"""
+co''\''mment
+"""
+asdf'''
+    assert trans.splitMultilineString(src) == ['qwer\n','"""\nco\'\'\'\'mment\n"""', '\nasdf']
+
+def testSplitMultilineString3():
+    src = '''qwer
 """
 comment
 """
-pass
-    '''
-    src_array = trans.splitComment(src)
-    assert src_array == ['\n', '# comment', '\npass\n   ', '# comment', '\npass ', '# comment', '\npass', '#comment', '\n\'\'\'\ncomment\n\'\'\'\npass\n"""\ncomment\n"""\npass\n    ']
+asdf
+"""
+comment
+"""
+asdf'''
+    assert trans.splitMultilineString(src) == ['qwer\n','"""\ncomment\n"""', '\nasdf\n','"""\ncomment\n"""', '\nasdf']
+
+def testSplitString1():
+    src='asdf"ab\'c"zxcv'
+    assert trans.splitString(src)== ['asdf', '"ab\'c"', 'zxcv']
+
+def testSplitString2():
+    src='asdf\'ab"c\'zxcv'
+    assert trans.splitString(src)== ['asdf', "'ab\"c'", 'zxcv']
+    
+def testSplitString3():
+    src='asdf"abc"zxcv\'abc\''
+    assert trans.splitString(src)== ['asdf', '"abc"', 'zxcv', "'abc'", '']
+    
+def testChangeKeywords1():
+    assert trans.changeKeywords('打印("你好,世界")') == 'print("你好,世界")'
+
+def testChangeKeywords2():
+    assert trans.changeKeywords('#打印("你好,世界")') == '#打印("你好,世界")'
+
+def testChangeKeywords3():
+    assert trans.changeKeywords('"打印("你好,世界")"') == '"打印("你好,世界")"'
+
+def testChangeKeywords4():
+    assert trans.changeKeywords('\'打印("你好,世界")\'') == '\'打印("你好,世界")\''
+
+
+def testChangeKeywords5():
+    assert trans.changeKeywords('打印好("你好,世界")') == '打印好("你好,世界")'
+
+
+
+
 
--- 源码/trans.py
+++ 源码/trans.py
@@ -2,23 +2,29 @@
 # -*- coding: UTF-8 -*-
 import sys
 import re
+
+gKeywordMap = [
+    [ '打印', 'print'],
+]
+
 def splitComment( str):
     return re.split(r'(#.*)', str, re.MULTILINE|re.S)
+
 def splitMultilineString( str):
-    return re.split(r'(?:("""|\'\'\')[\s\S]+?\1|("|\')(?:\\.|(?!\2)[^\\\r\n])*\2)', str, re.MULTILINE|re.S)
-    
+    r = r'(?:("""|\'\'\')[\s\S]+?\1|("|\')(?:\\.|(?!\2)[^\\\r\n])*\2)'
+    return re.split(r'("""[\s\S]+?"""|\'\'\'[\s\S]+?\'\'\')', str, re.MULTILINE|re.S)
+
+def splitString(str):
+    return re.split(r'(\'[\s\S]+?\'|"[\s\S]+?")', str, re.MULTILINE|re.S)
+
+def changeKeywords(str):
+    if re.match(r'[\'"#]', str) != None:
+        return str
+    for keywordPair in gKeywordMap:
+        str = re.sub(re.compile('(\W)' + keywordPair[0] + '(\W)', re.L|re.M), '\1' + keywordPair[1] + '\2')
+    return str
 
 def read( filename):
     return open(sys.argv[1], 'r', encoding='utf-8').read()
 
-        
-src = '''
-pass
-"""
-comment
-comment
-"""
-pass
-    '''
-src_array = splitMultilineString(src)
-print(src_array)
+print(changeKeywords('#打印("你好,世界")'))
